da86de4926ba7104a0ea5e59ea1b43cfe87ad677
Alexander Pustosmekhov <a.pustosmekhov@gmail.com>
2022-07-23 14:47:46 +0300

    fixes bugs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,52 +9,55 @@ import Contacts from './Contacts/Contacts';
 import PrivateRoute from './Route/PrivateRoute';
 import PublicRoute from './Route/PublicRoute';
 import { getIsFetchingCurrent } from 'redux/users/users.selector';
+import { getName } from '../redux/users/users.selector';
 
 const { fetchCurrentUser } = userOperations;
 
 function App() {
   const isFetching = useSelector(getIsFetchingCurrent);
   const dispatch = useDispatch();
+  const userName = useSelector(getName);
 
   useEffect(() => {
     dispatch(fetchCurrentUser());
   }, [dispatch]);
+
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Header />}>
+    !isFetching && (
+      <>
+        <Header />
+        <Routes>
+          <Route
+            index
+            // path="login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="register"
+            element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="contacts"
+            element={
+              <PrivateRoute>
+                <Contacts />
+              </PrivateRoute>
+            }
+          />
           {!isFetching && (
-            <>
-              <Route
-                path="login"
-                element={
-                  <PublicRoute>
-                    <Login />
-                  </PublicRoute>
-                }
-              />
-              <Route
-                path="register"
-                element={
-                  <PublicRoute>
-                    <Register />
-                  </PublicRoute>
-                }
-              />
-              <Route
-                path="contacts"
-                element={
-                  <PrivateRoute>
-                    <Contacts />
-                  </PrivateRoute>
-                }
-              />
-            </>
+            <Route path="*" element={userName ? <Contacts /> : <Login />} />
           )}
-        </Route>
-        {!isFetching && <Route path="*" element={<p>Page not found</p>} />}
-      </Routes>
-    </>
+        </Routes>
+      </>
+    )
   );
 }
 
--- src/components/Route/PrivateRoute.js
+++ src/components/Route/PrivateRoute.js
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { getIsLoggedIn } from 'redux/users/users.selector';
 import { Navigate } from 'react-router-dom';
 
-export default function PrivateRoute({ children, redirect = '/register' }) {
+export default function PrivateRoute({ children, redirect = '/login' }) {
   const isLogedIn = useSelector(getIsLoggedIn);
   return isLogedIn ? children : <Navigate to={redirect} replace />;
 }
